2ec106f3fc2a1a32abd75208014e28d5a11e3039
aleksandr.sazonov <karl93rus@gmail.com>
2020-08-09 17:35:16 +0300

    gitignore app

--- app/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./src/server/server");
-const server = new server_1.Server(3000);
-server.startServer();
--- app/src/server/routes/testroute.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const testhandler_1 = require("../handlers/testhandler");
-const testRouter = express_1.Router();
-exports.test = testRouter.get('/avi', testhandler_1.testHandler);
